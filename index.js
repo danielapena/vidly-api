ff0408cc9aee626359c3dc3d0c60e2b60f5b4cf9
Daniela Pena <danielan.pena@gmail.com>
2022-05-29 23:25:44 -0300

    refactor: :recycle: extract config to startup folder

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
@@ -8,11 +7,7 @@ const app = express();
 require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: JWT private key not defined");
-  process.exit(1);
-}
+require("./startup/config")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
